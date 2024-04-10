861c81c2341316d03cc474e1bf515f423c944ab1
Nataliia Svystula <105055646+Nataliia0202@users.noreply.github.com>
2024-04-10 10:19:34 +0300

    deleted import

--- src/pages/MainPage/ManePage.jsx
+++ src/pages/MainPage/ManePage.jsx
@@ -4,7 +4,7 @@ import { Button } from "components/Button/Button";
 import { EventList } from "components/Events/EventList";
 import { Container } from "components/GlobalStyles";
 import Pagination from "components/Pagination";
-import { WrapperSection } from "components/GlobalStyles";
+// import { WrapperSection } from "components/GlobalStyles";
 
 export const ManePage = () => {
   
